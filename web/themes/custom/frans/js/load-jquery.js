bfc31c6f15b6cead935a8970b648abe5647bf9c9
Boris Doesborg <bdoesborg@gmail.com>
2017-08-29 10:15:34 +0200

    wip

---
+++ web/themes/custom/frans/js/load-jquery.js
@@ -0,0 +1,3 @@
+/**
+ * Created by boris on 24-8-17.
+ */
--- web/themes/earthen/js/responsive-menu.js
+++
@@ -1,9 +0,0 @@
-/* Toggles between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function openResponsiveMenu() {
-  var x = document.getElementById("responsive-menu");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
